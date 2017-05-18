89a359bdca57a187f02e403d92eddf1b5c0acd22
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-05-18 15:51:59 +0900

    refactoring strategy

--- strategy.rb
+++ strategy.rb
@@ -22,31 +22,25 @@ module State
       [2,2]
   ]
 
-  def self.all
-    e = Enumerator.new do |y|
-      A_STATES.each do |a|
-        BC_STATES.each do |b|
-          y << a+b
-        end
-      end
-    end
-    e
+  ALL_STATES = A_STATES.product(BC_STATES).map {|a,bc| (a+bc).freeze }.freeze
+
+  def self.valid?(state)
+    ALL_STATES.include?( state )
   end
 
-  def self.valid?
-    A_STATES.include?( @state[0..1] ) and BC_STATES.include?( @state[2..3] )
+  def self.index( state )
+    ALL_STATES.index( state )
   end
-
 end
 
 class Strategy
 
   def initialize( actions )
-    @strategy = Hash[ State.all.zip( actions ) ]
+    @strategy = Hash[ State::ALL_STATES.zip( actions ) ]
   end
 
   def to_bits
-    State.all.map do |stat|
+    State::ALL_STATES.map do |stat|
       @strategy[stat] == :c ? 'c' : 'd'
     end.join
   end
@@ -56,33 +50,44 @@ class Strategy
   end
 end
 
-class StrategySpace
+class StrategyEnumerator
 
   def initialize
-    n = State.all.to_a.size
-    possible_actions = [[:c,:d]]*n
-    @space = Hash[ State.all.zip( possible_actions ) ]
+    n = State::ALL_STATES.size
+    @fixed_actions = {}
   end
 
-  def size
-    action_nums = @space.map do |_,actions|
-      actions.size
-    end
-    action_nums.inject(1, :*)
+  def set_fixed_action( state, action )
+    @fixed_actions[ state ] = action
+  end
+
+  def fixed_actions_to_bit
+    State::ALL_STATES.map do |stat|
+      @fixed_actions[stat] || '-'
+    end.join
   end
 
   def all_strategy
-    states = State.all.to_a
+    all_states = State::ALL_STATES
     e = Enumerator.new do |y|
-      actions = Array.new( states.size, nil )
-      iterate_for = lambda do |stat_idx|
-        stat = states[stat_idx]
-        @space[stat].each do |act|
+      # set fixed actions
+      actions = Array.new( all_states.size, nil )
+      @fixed_actions.each do |stat, act|
+        idx = State.index(stat)
+        actions[idx] = act
+      end
+
+      unfixed_states = all_states - @fixed_actions.keys
+      unfixed_state_indexes = unfixed_states.sort.map {|stat| State.index(stat) }
+
+      iterate_for = lambda do |idx|
+        stat_idx = unfixed_state_indexes[idx]
+        [:c,:d].each do |act|
           actions[stat_idx] = act
-          if stat_idx < states.size-1
-            iterate_for.call(stat_idx+1)
+          if idx < unfixed_state_indexes.size-1
+            iterate_for.call( idx+1 )
           else
-            pp stat_idx
+            #pp stat_idx
             y << Strategy.new(actions)
           end
         end
@@ -92,20 +97,50 @@ class StrategySpace
     end
     e
   end
-
 end
 
 if __FILE__ == $0
-  State.all.each {|s| p s}
-  p State.all.to_a.size
+  #State.all.each {|s| p s}
+  p State::ALL_STATES
+  p State::ALL_STATES.size
   p s = Strategy.new( [:c,:d]*20 )
   p s.valid?
-  p ss = StrategySpace.new
-  p ss.size
+  p se = StrategyEnumerator.new
   count = 0
-  ss.all_strategy.each do |stra|
+  FIXED_ACTIONS = {
+      [:d,:d,2,2] => :d,
+      [:d,:d,2,1] => :d,
+      [:d,:d,1,2] => :d,
+      [:d,:d,1,-1] => :d,
+      [:c,:d,2,2] => :d,
+      [:c,:d,1,-1] => :d,
+      [:c,:c,2,2] => :d,
+      [:c,:c,1,-1] => :d,
+      [:c,:d,1,2] => :d,
+      [:c,:d,2,1] => :d,
+      [:c,:c,0,0] => :c,
+      [:c,:c,1,1] => :d
+  }
+  FIXED_ACTIONS.each do |stat,act|
+    se.set_fixed_action(stat, act )
+  end
+
+  LIMITED_ACTIONS = {
+      states: [ [:c,:d,0,0], [:d,:c,0,0], [:d,:d,0,0] ],
+      possible_actions: [
+          [:c,:c,:d],
+          [:c,:d,:d],
+          [:d,:c,:d],
+          [:d,:d,:d],
+          [:c,:d,:c],
+          [:d,:d,:c]
+      ]
+  }
+  p se.fixed_actions_to_bit
+  #se.set_fixed_action([:d,:d,2,2], :d )
+  se.all_strategy.each do |stra|
     pp stra.to_bits
     count += 1
-    break if count > 100
+    break if count > 50
   end
 end
