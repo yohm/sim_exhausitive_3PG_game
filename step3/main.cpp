78876fc92c4b1986cf5537f4b8147a379b2381ff
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-05-31 09:01:27 +0900

    adopted another interface for debugging

--- step3/main.cpp
+++ step3/main.cpp
@@ -4,52 +4,56 @@
 #include "strategy.hpp"
 #include "Game.hpp"
 
-bool IsEfficient(const Strategy& str, double e) {
+double Payoff(const Strategy& str, double e) {
   Game g(str, str, str);
-  umatrix_t m;
 
   //double e = 0.01;
   const double r = 2.0;
   const double c = 1.0;
   auto fs = g.AveragePayoffs(e,r,c,1024);
   double fa = std::get<0>(fs);
-  std::cerr << fa <<std::endl;
+  return fa;
+}
 
-  /*
-  e = 0.005;
-  fs = g.AveragePayoffs(e,r,c,1024);
-  fa = std::get<0>(fs);
-  std::cout << fa <<std::endl;
-   */
-
-  if( fa < 0.9 /*(c*r-c) - 10*e */ ) { return false; }
+bool IsEfficient(const Strategy& str, double e, double th) {
+  double p = Payoff(str,e);
+  if( p < th ) { return false; }
   else { return true; }
 }
 
 int main(int argc, char** argv) {
 
-  if(argc != 3) {
+  if(argc != 3 && argc != 4) {
     std::cerr << "Error: invalid argument" << std::endl;
-    std::cerr << "  Usage: " << argv[0] << " <e> <input_strategies.txt>" << std::endl;
+    std::cerr << "  Usage : " << argv[0] << " <e> <input_strategy_string>" << std::endl;
+    std::cerr << "     or : " << argv[0] << " <e> <th> <input_strategies_file>" << std::endl;
     throw "invalid argument";
   }
 
-  double e = atof(argv[1]);
-  std::ifstream fin(argv[2]);
+  if( argc == 3 ) {
+    double e = atof(argv[1]);
+    const Strategy str( argv[2] );
+    double p = Payoff(str,e);
+    std::cout << p << std::endl;
+  }
+  else if( argc == 4) {
+    double e = atof(argv[1]);
+    double th = atof(argv[2]);
+    std::ifstream fin(argv[3]);
+    std::string line;
+    int count = 0;
+    while( std::getline(fin,line) ) {
+      //std::cout << line << line.size() << std::endl;
 
-  std::string line;
-  int count = 0;
-  while( std::getline(fin,line) ) {
-    //std::cout << line << line.size() << std::endl;
-
-    const Strategy str( line.c_str() );
-    //std::cout << str.toString() << std::endl;
-    //std::cout << str.toFullString() << std::endl;
-    if( IsEfficient(str,e) ) {
-      std::cout << str.toString() << std::endl;
+      const Strategy str( line.c_str() );
+      //std::cout << str.toString() << std::endl;
+      //std::cout << str.toFullString() << std::endl;
+      if( IsEfficient(str,e,th) ) {
+        std::cout << str.toString() << std::endl;
+      }
+      count++;
+      if( count % 1000 == 0 ) { std::cerr << "count: " << count << std::endl; }
     }
-    count++;
-    if( count % 1000 == 0 ) { std::cerr << "count: " << count << std::endl; }
   }
 
   return 0;
