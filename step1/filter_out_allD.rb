2ce5ffc0e078cc989f3e849d5700a4f7548375e8
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-06-06 23:14:33 +0900

    changed the spec of enumerator

--- step1/filter_out_allD.rb
+++ step1/filter_out_allD.rb
@@ -1,12 +1,6 @@
 require 'pp'
 require_relative 'graph'
-
-A_STATES = [
-    [:c,:c],
-    [:c,:d],
-    [:d,:c],
-    [:d,:d]
-]
+require_relative 'strategy'
 
 # Since either B or C is allD, the states cannot be [*,0] or [0,*], or [1,1].
 BC_STATES = [
@@ -22,13 +16,13 @@ BC_STATES = [
     [2,2]
 ]
 
-STATES = []
-A_STATES.each do |a|
+SUB_STATES = []
+State::A_STATES.each do |a|
   BC_STATES.each do |b|
-    STATES << a+b
+    SUB_STATES << a+b
   end
 end
-pp "STATES : ", STATES
+pp "SUB_STATES : ", SUB_STATES
 
 FIXED_STRATEGY = {
     [:d,:d,2, 2] => :d,
@@ -39,13 +33,13 @@ FIXED_STRATEGY = {
     [:c,:d,1,-1] => :d,
     [:c,:c,2, 2] => :d,
     [:c,:c,1,-1] => :d,
-    [:c,:c,0, 0] => :c,
-    [:c,:c,1, 1] => :d,
-    [:c,:d,1, 2] => :d,
-    [:c,:d,2, 1] => :d
+    #[:c,:c,0, 0] => :c,
+    #[:c,:c,1, 1] => :d,
+    #[:c,:d,1, 2] => :d,
+    #[:c,:d,2, 1] => :d
 }
 
-UNFIXED_STATES = STATES - FIXED_STRATEGY.keys
+UNFIXED_STATES = SUB_STATES - FIXED_STRATEGY.keys
 # pp "UNFIXED_STATES: ", UNFIXED_STATES
 
 strategy_candidates = [FIXED_STRATEGY]
@@ -82,12 +76,12 @@ def possible_next_states( strategy, stat )
 end
 
 def construct_transition_graph(strategy)
-  g = DirectedGraph.new( STATES.size )
+  g = DirectedGraph.new(SUB_STATES.size )
   #pp strategy
-  STATES.each_with_index do |stat,idx|
+  SUB_STATES.each_with_index do |stat,idx|
     next_states = possible_next_states( strategy, stat )
     #pp "stat", stat, "next_stat", next_states
-    next_indexes = next_states.map {|next_stat| STATES.index(next_stat) }
+    next_indexes = next_states.map {|next_stat| SUB_STATES.index(next_stat) }
     #pp next_indexes
     next_indexes.each do |n|
       g.add_link(idx, n)
@@ -102,18 +96,19 @@ strategy_candidates.each do |str|
   g = construct_transition_graph(str)
   #pp g
   risky_state = g.non_transient_nodes.find do |n|
-    s = STATES[n]
+    s = SUB_STATES[n]
     !(s[0] == :d and s[1] == :d)
   end
   #pp "risky_state : #{risky_state}"
   is_defensible = risky_state.nil?
   #$stderr.puts "strategy #{str} is #{is_defensible ? 'defensible':'non-defensible'}"
   defensible_strategies << str if is_defensible
-  #break #TODO : remove later
 end
 
 def strategy_to_bits( strategy )
-  acts
+  State::ALL_STATES.map do |stat|
+    (strategy[stat] || '-').to_s
+  end.join
 end
 
 pp "# defensible : #{defensible_strategies.count}"
@@ -127,3 +122,7 @@ pp defensible_strategies.map {|str|
     end
   }
 
+pp "bits"
+defensible_strategies.each do |str|
+  puts strategy_to_bits(str)
+end
--- step1/strategy_enumerator.rb
+++ step1/strategy_enumerator.rb
@@ -3,183 +3,75 @@ require 'pp'
 
 class StrategyEnumerator
 
-  FIXED_ACTIONS = {
-      [:d,:d,2,2] => :d,
-      [:d,:d,2,1] => :d,
-      [:d,:d,1,2] => :d,
-      [:d,:d,1,-1] => :d,
-      [:c,:d,2,2] => :d,
-      [:c,:d,1,-1] => :d,
-      [:c,:c,2,2] => :d,
-      [:c,:c,1,-1] => :d,
-      [:c,:d,1,2] => :d,
-      [:c,:d,2,1] => :d,
-      [:c,:c,0,0] => :c,
-      [:c,:c,1,1] => :d
-  }
-
-  LIMITED_ACTIONS1 = {
-      states: [ [:c,:d,0,0], [:d,:c,0,0], [:d,:d,0,0] ],
-      possible_actions: [
-          [:c,:c,:d],
-          [:c,:d,:d],
-          [:d,:c,:d],
-          [:d,:d,:d],
-          [:c,:d,:c],
-          [:d,:d,:c]
-      ]
-  }
-
-  LIMITED_ACTIONS2 = {
-      states: [
-          [:c, :c, 1, 2],
-          [:c, :c, 2, 1],
-          [:d, :c, 1, -1],
-          [:d, :c, 1, 2],
-          [:d, :c, 2, 1],
-          [:d, :c, 2, 2]
-      ],
-      possible_actions: [
-          [:d, :c, :c, :c, :c, :c],
-          [:c, :d, :c, :c, :c, :c],
-          [:d, :d, :c, :c, :c, :c],
-          [:d, :c, :d, :c, :c, :c],
-          [:c, :d, :d, :c, :c, :c],
-          [:d, :d, :d, :c, :c, :c],
-          [:d, :c, :c, :d, :c, :c],
-          [:c, :d, :c, :d, :c, :c],
-          [:d, :d, :c, :d, :c, :c],
-          [:d, :c, :d, :d, :c, :c],
-          [:c, :d, :d, :d, :c, :c],
-          [:d, :d, :d, :d, :c, :c],
-          [:d, :c, :c, :c, :d, :c],
-          [:c, :d, :c, :c, :d, :c],
-          [:d, :d, :c, :c, :d, :c],
-          [:d, :c, :d, :c, :d, :c],
-          [:c, :d, :d, :c, :d, :c],
-          [:d, :d, :d, :c, :d, :c],
-          [:d, :c, :c, :d, :d, :c],
-          [:c, :d, :c, :d, :d, :c],
-          [:d, :d, :c, :d, :d, :c],
-          [:d, :c, :d, :d, :d, :c],
-          [:c, :d, :d, :d, :d, :c],
-          [:d, :d, :d, :d, :d, :c],
-          [:d, :c, :c, :c, :c, :d],
-          [:c, :d, :c, :c, :c, :d],
-          [:d, :d, :c, :c, :c, :d],
-          [:d, :c, :d, :c, :c, :d],
-          [:c, :d, :d, :c, :c, :d],
-          [:d, :d, :d, :c, :c, :d],
-          [:d, :c, :c, :d, :c, :d],
-          [:c, :d, :c, :d, :c, :d],
-          [:d, :d, :c, :d, :c, :d],
-          [:d, :c, :d, :d, :c, :d],
-          [:c, :d, :d, :d, :c, :d],
-          [:d, :d, :d, :d, :c, :d],
-          [:d, :c, :c, :c, :d, :d],
-          [:c, :d, :c, :c, :d, :d],
-          [:d, :d, :c, :c, :d, :d],
-          [:d, :c, :d, :c, :d, :d],
-          [:c, :d, :d, :c, :d, :d],
-          [:d, :d, :d, :c, :d, :d],
-          [:d, :c, :c, :d, :d, :d],
-          [:c, :d, :c, :d, :d, :d],
-          [:d, :d, :c, :d, :d, :d],
-          [:d, :c, :d, :d, :d, :d],
-          [:c, :d, :d, :d, :d, :d],
-          [:d, :d, :d, :d, :d, :d]
-      ]
-  }
-
-  def initialize
-    n = State::ALL_STATES.size
+  def initialize(pattern)
+    @fixed_actions = pattern.chars.map do |c|
+      case c
+      when 'd'
+        :d
+      when 'c'
+        :c
+      when '-'
+        nil
+      else
+        raise StandardError("invalid pattern")
+      end
+    end
+    @fixed_actions.freeze
   end
 
-  def fixed_actions_to_bit
-    State::ALL_STATES.map do |stat|
-      if FIXED_ACTIONS[stat]
-        FIXED_ACTIONS[stat]
-      elsif LIMITED_ACTIONS1[:states].include?(stat)
-        '*'
-      elsif LIMITED_ACTIONS2[:states].include?(stat)
-        '#'
-      else
-        '-'
-      end
+  def to_bit
+    @fixed_actions.map do |act|
+      act ? act.to_s : '-'
     end.join
   end
 
+  def size
+    num_nil = @fixed_actions.count(nil)
+    2 ** num_nil
+  end
+
   def all_strategy
-    actions = Array.new( State::ALL_STATES.size, nil )
+    actions = @fixed_actions.dup
 
     e = Enumerator.new do |y|
-      # set fixed actions
-      FIXED_ACTIONS.each do |stat, act|
-        idx = State.index(stat)
-        actions[idx] = act
-      end
 
-      # iterate on limited actions
-      i1,i2,i3 = LIMITED_ACTIONS1[:states].map {|s| State.index(s)}
-      LIMITED_ACTIONS1[:possible_actions].reverse.each do |a1,a2,a3|
-        actions[i1] = a1
-        actions[i2] = a2
-        actions[i3] = a3
-
-        i4,i5,i6,i7,i8,i9 = LIMITED_ACTIONS2[:states].map {|s| State.index(s)}
-        LIMITED_ACTIONS2[:possible_actions].reverse.each do |a4,a5,a6,a7,a8,a9|
-          actions[i4] = a4
-          actions[i5] = a5
-          actions[i6] = a6
-          actions[i7] = a7
-          actions[i8] = a8
-          actions[i9] = a9
-
-          unfixed_states = State::ALL_STATES - FIXED_ACTIONS.keys - LIMITED_ACTIONS1[:states] - LIMITED_ACTIONS2[:states]
-          unfixed_state_indexes = unfixed_states.sort.map {|stat| State.index(stat) }
-
-          iterate_for = lambda do |idx|
-            stat_idx = unfixed_state_indexes[idx]
-            [:c,:d].each do |act|
-              actions[stat_idx] = act
-              if idx < unfixed_state_indexes.size-1
-                iterate_for.call( idx+1 )
-              else
-                #pp stat_idx
-                y << Strategy.new(actions)
-              end
-            end
+      iterate_for = lambda do |idx|
+        possible_actions = []
+        if act = @fixed_actions[idx]
+          possible_actions = [ act ]
+        else
+          possible_actions = [:c,:d]
+        end
+        possible_actions.each do |act|
+          actions[idx] = act
+          if idx < @fixed_actions.size-1
+            iterate_for.call(idx+1)
+          else
+            y << actions
           end
-          iterate_for.call(0)
         end
       end
+      iterate_for.call(0)
     end
-    e
   end
+
 end
 
 if __FILE__ == $0
-  #p State::ALL_STATES
-  #p State::ALL_STATES.size
-  #p s = Strategy.new( [:c,:d]*20 )
-  #p s.valid?
   unless ARGV.size == 1
-    $stderr.puts "usage: ruby #{__FILE__} outfile"
+    $stderr.puts "usage: ruby #{__FILE__} pattern"
     raise "invalid argument"
   end
-  outfile = ARGV[0]
-  se = StrategyEnumerator.new
-  $stderr.puts se.fixed_actions_to_bit
+  pattern = ARGV[0]
+  se = StrategyEnumerator.new(pattern)
+  $stderr.puts se.to_bit, total = se.size
   count = 0
-  total_num_strategies = (2**28)*9/16
-  io = File.open(outfile,'w')
   se.all_strategy.each do |stra|
-    io.puts stra.to_bits
+    $stdout.puts stra.map(&:to_s).join
     count += 1
     if count % 1_000_000 == 0
-      $stderr.puts "count: #{count} / #{total_num_strategies}"
-      #break
+      $stderr.puts "count: #{count} / #{total}"
     end
   end
-  io.close
+  $stdout.flush
 end
