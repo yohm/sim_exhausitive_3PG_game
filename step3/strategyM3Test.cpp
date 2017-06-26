d19b44e49cbaf8d8210b7e3dbd8db4e2c0820118
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-06-26 16:06:45 +0900

    make tests richer

--- step3/strategyM3Test.cpp
+++ step3/strategyM3Test.cpp
@@ -7,6 +7,12 @@
 #include <cassert>
 #include "StrategyM3.hpp"
 
+void expect(bool b) {
+  if(b==false) {
+    throw "test failed";
+  }
+}
+
 void test_StateM3() {
 
   FullStateM3 s(D,D,C,D,D,D,C,C,C);
@@ -20,19 +26,19 @@ void test_StateM3() {
   std::cout << "from C: " << s.FromC();
   std::cout << std::endl;
 
-  assert( s.NumDiffInT1(s) == 0 );
+  expect( s.NumDiffInT1(s) == 0 );
 
   FullStateM3 s2(D,D,D,D,D,D,C,C,C);
-  assert( s.NumDiffInT1(s2) == 1 );
+  expect( s.NumDiffInT1(s2) == 1 );
 
   FullStateM3 s3(D,D,D,D,D,C,C,C,C);
-  assert( s.NumDiffInT1(s3) == 2 );
+  expect( s.NumDiffInT1(s3) == 2 );
 
   FullStateM3 s4(D,D,D,D,D,C,C,C,D);
-  assert( s.NumDiffInT1(s4) == 3 );
+  expect( s.NumDiffInT1(s4) == 3 );
 
   FullStateM3 s5(D,D,C,D,D,D,C,D,C);
-  assert( s.NumDiffInT1(s5) == -1 );
+  expect( s.NumDiffInT1(s5) == -1 );
 }
 
 void test_StrategyM3() {
@@ -58,15 +64,16 @@ void test_TransitionGraph() {
 
   StrategyM3 str(acts);
   Graph g = str.TransitionGraph();
-  assert( g.m_num_nodes == 512 );
-  assert( g.m_links.size() == 512 );
+  expect( g.m_num_nodes == 512 );
+  expect( g.m_links.size() == 512 );
   for( const auto& links_i : g.m_links ) {
-    assert(links_i.size() == 4);
+    expect(links_i.size() == 4);
   }
 
 }
 
 void test_Defensible() {
+  std::cerr << "test defensibility" << std::endl;
   const char* acts =
       "cdcdcdcdddccdddccccdcdcddcdddcddcdcdcdcddddcdddccdcdcdcddcdddcddccddcdddcdcdddcddcdddcddddddddddcdddcdddddcddd"
           "cddcdddcdddddddddddccdddcdcccddcddcccccdccddccddccddcdddcddcdddcddcdcccdccddccddccccddccddccddcdddddddcddddddd"
@@ -75,17 +82,25 @@ void test_Defensible() {
           "ddccddccccddccddcdddcdddddddddddddddddddccddccddcdddcddddddddddddddddddd";
 
   StrategyM3 str(acts);
-  std::cout << str << std::endl;
-  std::cout << "  is defensible?" << str.IsDefensible() << std::endl;
+  expect( str.IsDefensible() == true );
 
+  std::cerr << "test defensibility allC" << std::endl;
   const char* actsC =
       "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
           "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
           "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
           "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc";
   StrategyM3 allC(actsC);
-  std::cout << allC << std::endl;
-  std::cout << "  is defensible?" << allC.IsDefensible() << std::endl;
+  expect( allC.IsDefensible() == false );
+
+  std::cerr << "test defensibility allD" << std::endl;
+  const char* actsD =
+      "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+          "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+          "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+          "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd";
+  StrategyM3 allD(actsD);
+  expect( allD.IsDefensible() == true );
 }
 
 int main() {
