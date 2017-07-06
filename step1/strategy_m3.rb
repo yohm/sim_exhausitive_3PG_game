a051acf858a079f4c3a2ab936bd50110a7be810e
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-07-06 11:12:59 +0900

    make m3_successful strategies

--- step1/strategy_m3.rb
+++ step1/strategy_m3.rb
@@ -45,7 +45,6 @@ class StrategyM3
   def modify_action( state, action )
     if state.is_a?(String)
       stat = FullStateM3.make_from_bits(state)
-      raise "action does not change: #{stat}" if @actions[stat.to_id] == action
       @actions[stat.to_id] = action
     elsif state.is_a?(FullStateM3)
       @actions[state.to_id] = action
@@ -83,6 +82,29 @@ class StrategyM3
       return false if( a_b.has_negative_diagonal? or a_c.has_negative_diagonal? )
     end
     true
+  end
+
+  def make_successful
+    # noise on B&C (state 0->5)
+    modify_action('ccdccdccc',:c) # (5->26)
+    modify_action('ccdcccccd',:c) # (5->26)
+    modify_action('cdccdcccd',:c) # (26->48)
+    modify_action('cdcccdcdc',:c) # (26->48)
+    # noise on B -> on B (state 4->29)
+    modify_action('cddccdccd',:c) # (29->59) B
+    modify_action('ddccddcdd',:c) # (59->34) B
+    modify_action('cddddccdd',:c) # (59->34) A or C
+    modify_action('cddcddddc',:c) # (59->34) A or C
+    # noise on B -> on C (state 4->24)
+    modify_action('cdcccdccc',:c) # (24->48)
+    modify_action('cdccccccd',:c) # (24->48)
+    modify_action('ccccdcccd',:c) # (24->48)
+    modify_action('cccccdcdc',:c) # (24->48)
+    # noise on B -> _ -> on B (state 25->38)
+    modify_action('dcdcdccdc',:c) # (38->25)
+    # noise on B -> _ -> on C (state25->35)
+    modify_action('cdddcccdc',:c) # (35->48)
+    modify_action('cddcdcdcc',:c) # (35->48)
   end
 
   class AMatrix  # class used for judging defensibility
@@ -159,52 +181,34 @@ class StrategyM3
 end
 
 if __FILE__ == $0
-  if ARGV.size == 1
-    bits = ARGV[0]
-    stra = Strategy.make_from_bits(bits)
-    stra.show_actions($stdout)
-    a1_b, a1_c = Strategy::AMatrix.construct_a1_matrix(stra)
-    pp a1_b
-    pp a1_b.has_negative_diagonal?
-    a1_b.update(a1_b)
-    pp a1_b
-    pp "def: #{stra.defensible?}"
+  def test
+    bits = "cddd" * 128
+    strategy = StrategyM3.make_from_bits(bits)
+    strategy.show_actions($stdout)
+    pp strategy.valid?, strategy.defensible?
+    raise "inconsistent bits" unless bits == strategy.to_bits
 
-    exit 0
+    bits = "cddcddccddcdddcdddddddccdddcccccdddddddd"
+    m2_stra = Strategy.make_from_bits(bits)
+    m3_stra = StrategyM3.make_from_m2_strategy(m2_stra)
   end
 
-  bits = "cddd" * 128
-  strategy = StrategyM3.make_from_bits(bits)
-  strategy.show_actions($stdout)
-  pp strategy.valid?, strategy.defensible?
-  raise "inconsistent bits" unless bits == strategy.to_bits
-
-  bits = "cddcddccddcdddcdddddddccdddcccccdddddddd"
-  m2_stra = Strategy.make_from_bits(bits)
-  m3_stra = StrategyM3.make_from_m2_strategy(m2_stra)
-
-  # noise on B&C
-  m3_stra.modify_action('ccdccdccc',:c)
-  m3_stra.modify_action('ccdcccccd',:c)
-  m3_stra.modify_action('cdccdcccd',:c)
-  m3_stra.modify_action('cdcccdcdc',:c)
-  # noise on B -> on B
-  m3_stra.modify_action('cddccdccd',:c)
-  m3_stra.modify_action('cddddccdd',:c)
-  m3_stra.modify_action('cddcddddc',:c)
-  # noise on B -> on C
-  m3_stra.modify_action('cdcccdccc',:c)
-  m3_stra.modify_action('cdccccccd',:c)
-  m3_stra.modify_action('ccccdcccd',:c)
-  m3_stra.modify_action('cccccdcdc',:c)
-  # noise on B -> _ -> on B
-  m3_stra.modify_action('dcdcdccdc',:c)
-  # noise on B -> _ -> on C
-  m3_stra.modify_action('cdddcccdc',:c)
-  m3_stra.modify_action('cddcdcdcc',:c)
-
-
-  m3_stra.show_actions($stdout)
-  puts m3_stra.to_bits
+  if ARGV.size == 1 and ARGV[0].length == 40
+    stra = Strategy.make_from_bits(ARGV[0])
+    m3_stra = StrategyM3.make_from_m2_strategy(stra)
+    m3_stra.show_actions($stdout)
+    puts m3_stra.to_bits
+  elsif ARGV.size == 1
+    File.open(ARGV[0]).each do |line|
+      stra = Strategy.make_from_bits(line.chomp)
+      m3_stra = StrategyM3.make_from_m2_strategy(stra)
+      m3_stra.make_successful
+      puts m3_stra.to_bits
+    end
+  else
+    $stdout.puts "[Error] Usage: ruby #{__FILE__} strategies.txt"
+    $stdout.puts "           or: ruby #{__FILE__} strategy"
+    raise "invalid argument"
+  end
 end
 
