a29bcb017dca04a288fb6f48edcbbf8f60ee72a2
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-07-22 21:28:34 +0300

    job scripts

--- cpp/job_efficiency_768.sh
+++ cpp/job_efficiency_768.sh
@@ -6,14 +6,14 @@
 #PJM --mpi "proc=6144"
 #PJM --stg-transfiles all
 #PJM --mpi "use-rankdir"
-#PJM --stgin  "rank=* ./defense1.out %r:./"
-#PJM --stgin  "rank=* ./split6144/defense1_split_%04r %r:./"
-#PJM --stgout "rank=* %r:./defense_out_%04r %j/"
+#PJM --stgin  "rank=* ./main_efficiency.out %r:./"
+#PJM --stgin  "rank=* ./defensible/defensible_%04r %r:./"
+#PJM --stgout "rank=* %r:./defensible_efficiency_%04r %j/"
 #PJM -s
 
 . /work/system/Env_base
 
 ulimit -s 8192
 
-mpiexec ./defense1.out defense1_split_%04d defense_out_%04d
+mpiexec ./main_efficiency.out 0.001 defensible_%04d defensible_efficiency_%04d
 
--- cpp/k_build.sh
+++ cpp/k_build.sh
@@ -1,3 +1,3 @@
-mpiFCCpx -Kfast -DNDEBUG -std=c++11 -o efficiency.out main_efficiency.cpp Strategy.cpp Game.cpp Graph.cpp
-mpiFCCpx -Kfast -DNDEBUG -std=c++11 -o defense1.out main_defense1.cpp Strategy.cpp Graph.cpp
+mpiFCCpx -Kfast -DNDEBUG -std=c++11 -o main_efficiency.out main_efficiency.cpp Strategy.cpp Game.cpp Graph.cpp
+mpiFCCpx -Kfast -DNDEBUG -std=c++11 -o main_defensible.out main_defensible.cpp Strategy.cpp Graph.cpp
 
