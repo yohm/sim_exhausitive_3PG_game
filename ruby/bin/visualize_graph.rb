e60c163f4ef7df916c23bd89dc77b930438dbf45
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-07-27 11:05:29 +0300

    generalize visualize_graph script to m3

--- ruby/bin/visualize_graph.rb
+++ ruby/bin/visualize_graph.rb
@@ -1,14 +1,20 @@
 require_relative "../lib/strategy"
+require_relative "../lib/strategy_m3"
 
 unless ARGV.size == 2
   $stderr.puts "[Error] Usage: ruby #{__FILE__} strategy_string output.pdf"
   raise "invalid argument"
 end
 
-stra = Strategy.make_from_bits(ARGV[0])
-g = stra.transition_graph_with_self
-
-node_attributes = Strategy.node_attributes
+if ARGV[0].size == 40
+  stra = Strategy.make_from_bits(ARGV[0])
+  g = stra.transition_graph_with_self
+  node_attributes = Strategy.node_attributes
+elsif ARGV[0].size == 512
+  stra = StrategyM3.make_from_bits(ARGV[0])
+  g = stra.transition_graph_with_self
+  node_attributes = StrategyM3.node_attributes
+end
 
 tmp = "temp.dot"
 io = File.open(tmp, 'w')
