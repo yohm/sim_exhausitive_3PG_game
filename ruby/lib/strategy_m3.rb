716a3a7069b7e2636a7bb2de452c4d316c861d74
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-01-17 17:41:51 +0900

    added test cases of PS2 and SS

--- ruby/lib/strategy_m3.rb
+++ ruby/lib/strategy_m3.rb
@@ -383,6 +383,82 @@ if __FILE__ == $0
       assert_equal all.uniq.sort, path.map(&:to_s).sort
     end
 
+    def test_recovery_PS2
+      # most generous version of PS2 extended to m=3
+      bits = "cdcdcdcdddddddddcdcdcdcdddddddddcdcdcdcdddddddddcdcdcdcdddddddddccddccddcccdcccddcdddcddddddddddccddccddcccdcccddcdddcdddddddddddccddccdccddccddcdcccdccddccddccdccddccdccddccddcdcccdccddccddccccddccddcdcdcdcddcdddcddddddddddccddccddcdcdcdcddcdddcddddddddddcdcdcdcdddddddddcdcdcdcdddddddddcdcdcdcdddddddddcdcdcdcdddddddddccddccddcccdcccddcdddcddddddddddccddccddcccdcccddcdddcdddddddddddccddccdccddccddcdcccdccddccddccdccddccdccddccddcdcccdccddccddccccddccddcdcdcdcddcdddcddddddddddccddccddcdcdcdcddcdddcdddddddddd"
+      stra = StrategyM3.make_from_bits(bits)
+
+      assert_equal ['ccc-ccc-ccc'], stra.recovery_path_nodes(0).map(&:to_s)
+
+      path = stra.recovery_path_nodes(1)
+      path_a = ['ccd-ccc-ccc','cdc-ccd-ccd','dcc-cdc-cdc','ccc-dcc-dcc','ccc-ccc-ccc']
+      path_b = swap_players(path_a,0,1)
+      path_c = swap_players(path_a,0,2)
+      assert_equal (path_a+path_b+path_c).uniq.sort, path.map(&:to_s).sort
+
+      assert_equal false, stra.recovery_path_nodes(2)
+    end
+
+    def test_recovery_SS
+      # most generous successful m=3 strategy
+      bits = "cdcdcdcdddcdddddcccdcdcdddddddddcdcdcdcdddddddddcdcdcdcdddddddddccddccddcccdcccddcdddcddddddddddccddccddcccdcccddcdddcdddddddddddccddccdcccdccddcccccdccddccddccdccddccdccddccddcdcccdccddccddccccddccddcccdcdcddcddccddddddddcdcccdccddcdcdcdcddcdcdcddddddddddcdcdcdcdddddddddcdcdcdcdddddddddcdcdcdcdddddddddcdcdcdcdddddddddccddccddcccdcccddccddcddddddddddccddccddcccdcccddcdddcdddddddddddccddccdccddccddcdcccdccddccddccdccddccdccddccddcdcccdccddccddccccddccddcdcdcdcddcdddcddddddddddccddccddcdcdcdcddcdddcdddddddddd"
+      stra = StrategyM3.make_from_bits(bits)
+
+      assert_equal ['ccc-ccc-ccc'], stra.recovery_path_nodes(0).map(&:to_s)
+      path = stra.recovery_path_nodes(1)
+      path_a = ['ccd-ccc-ccc','cdc-ccd-ccd','dcc-cdc-cdc','ccc-dcc-dcc','ccc-ccc-ccc']
+      path_b = swap_players(path_a,0,1)
+      path_c = swap_players(path_a,0,2)
+      assert_equal (path_a+path_b+path_c).uniq.sort, path.map(&:to_s).sort
+
+      path = stra.recovery_path_nodes(2)
+      path_bc = ['ccc-ccd-ccd','ccd-cdc-cdc','cdd-dcc-dcc','ddc-ccd-ccd','dcc-cdc-cdc','ccc-dcc-dcc','ccc-ccc-ccc']
+      path_ab = swap_players(path_bc,0,2)
+      path_ac = swap_players(path_ab,1,2)
+
+      path_b_b = ['ccc-ccd-ccc','ccd-cdd-ccd','cdd-ddc-cdd','ddc-dcc-ddc','dcc-ccc-dcc','ccc-ccc-ccc']
+      path_b_c = ['ccc-ccd-ccc','ccd-cdc-ccc','cdd-dcc-ccc','ddc-ccd-ccd','dcc-cdc-cdc','ccc-dcc-dcc','ccc-ccc-ccc']
+      path_a_a = swap_players(path_b_b,0,1)
+      path_a_b = swap_players(swap_players(path_b_c,0,1),1,2)
+      path_a_c = swap_players(path_a_b,1,2)
+      path_b_a = swap_players(path_a_b,0,1)
+      #path_b_b = swap_players(path_a_a,0,1)
+      #path_b_c = swap_players(path_b_a,0,2)
+      path_c_a = swap_players(path_a_c,0,2)
+      path_c_b = swap_players(path_c_a,0,1)
+      path_c_c = swap_players(path_a_a,0,2)
+
+      path_b__b = ['ccc-ccd-ccc','ccd-cdc-ccd','cdc-dcd-cdc','dcd-cdc-dcd','cdc-dcc-cdc','dcc-ccc-dcc','ccc-ccc-ccc']
+      path_b__c = ['ccc-ccd-ccc','ccd-cdc-ccd','cdc-dcc-cdd','dcd-ccd-ddc','cdc-cdc-dcc','dcc-dcc-ccc','ccc-ccc-ccc']
+      path_a__a = swap_players(path_b__b,0,1)
+      path_a__b = swap_players(swap_players(path_b__c,1,2), 0,2)
+      path_a__c = swap_players(path_a__b,1,2)
+      path_b__a = swap_players(path_a__b,0,1)
+      #path_b__b = swap_players(path_a__a,0,1)
+      #path_b__c = swap_players(path_b__a,0,2)
+      path_c__a = swap_players(path_b__a,1,2)
+      path_c__b = swap_players(path_c__a,0,1)
+      path_c__c = swap_players(path_a__a,0,2)
+
+      path_b___b = ['ccc-ccd-ccc','ccd-cdc-ccd','cdc-dcc-cdc','dcc-ccd-dcc','ccd-cdc-ccd','cdc-dcc-cdc','dcc-ccc-dcc','ccc-ccc-ccc']
+      path_a___a = swap_players(path_b___b,0,1)
+      path_c___c = swap_players(path_a___a,0,2)
+      path_b___c = ['ccc-ccd-ccc','ccd-cdc-ccd','cdc-dcc-cdc','dcc-ccc-dcd','ccd-ccd-cdc','cdc-cdc-dcc','dcc-dcc-ccc','ccc-ccc-ccc']
+      path_b___a = swap_players(path_b___c,0,2)
+      path_a___c = swap_players(path_b___c,0,1)
+      path_a___b = swap_players(path_a___c,1,2)
+      path_c___a = swap_players(path_b___a,1,2)
+      path_c___b = swap_players(path_c___a,0,1)
+
+      all = (path_a+path_b+path_c+
+          path_ab+path_ac+path_bc+path_a_a+path_a_b+path_a_c+path_b_a+path_b_b+path_b_c+
+          path_c_a+path_c_b+path_c_c+path_a__a+path_a__b+path_a__c+path_b__a+path_b__b+path_b__c+path_c__a+path_c__b+path_c__c+
+          path_a___a+path_a___b+path_a___c+path_b___a+path_b___b+path_b___c+path_c___a+path_c___b+path_c___c).uniq
+      assert_equal [], (path.map(&:to_s)-all).sort
+      assert_equal [], (all-path.map(&:to_s)).sort
+      assert_equal all.uniq.sort, path.map(&:to_s).uniq.sort
+    end
+
     def swap_players(states, p1 = 0, p2 = 1)
       states.map {|state|
         splitted = state.split('-')
